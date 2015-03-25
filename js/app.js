23e3dfbf1d41662e73bb9085ec219a00c31490b4
akboada <crosseyedjonny@yahoo.com>
2015-03-25 16:42:53 -0700

    change font, fix JS formatting, fix enemy x positioning

--- js/app.js
+++ js/app.js
@@ -2,24 +2,26 @@
 var gameWidth = 400;
 var gameHeight = 400;
 
+//sets score and lives
 var scoreCounter = 0;
 var livesCounter = 3;
 
+//Enemy
+
 var Enemy = function() {
     this.sprite = 'images/enemy-bug.png';
-    this.x = 50;
-    this.yPos = [50, 130, 225];
-    //https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random 
+    this.x = -100;
+    this.yPos = [50, 130, 225]; 
+    this.height = 50;
+    this.width = 50;
     this.speed = Math.floor(Math.random() * (300 - 200)) + 200;
 }
     
-
 Enemy.prototype.update = function(dt) {
+//advances enemy
     this.x += this.speed * dt;
 
-    this.height = 50;
-    this.width = 50;
-
+//detects collision with player
     if (this.x < player.x + player.width &&
         this.x + this.width > player.x &&
         this.y < player.y + player.height &&
@@ -28,11 +30,10 @@ Enemy.prototype.update = function(dt) {
         livesCounter--;
     }
 
+//respawns enemies after passing the edge of the canvas
     if (this.x > gameWidth + 100){
         this.reset();
     }
-
-    
 }
 
 Enemy.prototype.render = function() {
@@ -40,7 +41,7 @@ Enemy.prototype.render = function() {
 }
 
 Enemy.prototype.reset = function() {
-    this.x = 10;
+    this.x = -100;
     this.y = this.yPos[Math.round(Math.random() * this.yPos.length)];
 }    
 
@@ -50,90 +51,87 @@ var Player = function() {
     this.sprite = 'images/char-boy.png';
     this.x = 200;
     this.y = 400;
+    this.height = 50;
+    this.width = 50;
     this.speed = 25;
 }
 
-
 Player.prototype.update = function() {
-    this.y = this.y;
-    
-    this.x = this.x;
-
-    this.height = 50;
-    this.width = 50;
-
-    if (this.y <= 3){
+//resets player once water is reached
+   if (this.y <= 3){
         this.reset();
         scoreCounter++;
-        console.log(scoreCounter);
+    }
+
+//Game Over Message
+    if (livesCounter < 1){
+        alert('Game Over');
+        livesCounter = 3;
+        scoreCounter = 0;
     }
 }
 
+//renders player sprite and scorekeeping/lives text
 
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 
-    ctx.font = "25px Futura";
-    ctx.textAlign = "right";
-    ctx.fillStyle= "white";
+    ctx.font = '25px Lato';
+    ctx.textAlign = 'right';
+    ctx.fillStyle= 'white';
 
     //Score Counter Text
-    ctx.fillText("Score:", 85, 85);
+    ctx.fillText('Score:', 85, 85);
     ctx.fillText(scoreCounter, 60, 115);
 
     //Lives Counter Text
-    ctx.fillText("Lives:", 485, 85);
+    ctx.fillText('Lives:', 485, 85);
     ctx.fillText(livesCounter, 460, 115);
-
-    if (livesCounter < 1){
-        alert("Game Over");
-        livesCounter = 3;
-        scoreCounter = 0;
-
-    }
 }
 
+//resets player to initial position
 
 Player.prototype.reset = function(){
     this.x = 200;
     this.y = 400;
 }
 
-Player.prototype.handleInput = function(key) {
+//handles user key input
 
+Player.prototype.handleInput = function(key) {
     switch (key) {
-        case "up":
+        case 'up':
             this.y = this.y - this.speed;
             break;
-        case "down":
+        case 'down':
             if (this.y < gameHeight) {
                 this.y = this.y + this.speed;
             }
             break;
-        case "left":
+        case 'left':
             if (this.x > 0){
                 this.x = this.x - this.speed;
             }
             break;
-        case "right":
+        case 'right':
             if (this.x < gameWidth){
             this.x = this.x + this.speed;    
             }
             break;
         default:
-            console.log("Please select an arrow key");                          
+            console.log('Please select an arrow key')                          
     }
 }
 
+//instatiates Enemies and Player
 
-
-var allEnemies = [enemy = new Enemy(), enemy2 = new Enemy(), enemy3 = new Enemy()];
+var allEnemies = [
+    enemy = new Enemy(), 
+    enemy2 = new Enemy(), 
+    enemy3 = new Enemy()
+];
 
 var player = new Player();
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
@@ -145,5 +143,3 @@ document.addEventListener('keyup', function(e) {
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
-
-
